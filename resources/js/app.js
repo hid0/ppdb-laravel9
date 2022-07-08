f3f8d39511128669e1718e59aa96d65d0a3f8ff4
hid0 <faizhid11@gmail.com>
2022-07-09 01:56:25 +0700

    adding resource sass

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+// Bootstrap's JS'
+const bootstrap = require('bootstrap');
+
+// Make bootstrap's js available via the window object
+// (Used in the tooltip example)
+window.bootstrap = bootstrap;
+
+// Feather icons are used on some pages
+// Replace() replaces [data-feather] elements with icons
+import featherIcons from "feather-icons"
+featherIcons.replace();
+
+// Mazer internal JS. Include this in your project to get
+// the sidebar running.
+require("./components/dark");
+require("./mazer");
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\{Route, Auth};
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('dashboard');
 });
 
 Auth::routes();
